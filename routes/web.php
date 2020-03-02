cfb63071d640d98d2cf64ab8d7d43b9b47ffbea2
karuyanakaido24@gmail.com <karuyanakaido24@gmail.com>
2020-03-02 11:28:01 +0700

    A simple example with route and how to send params Signed-off-by: karuyanakaido24@gmail.com <karuyanakaido24@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,17 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// route with view
 Route::get('/', function () {
     return view('welcome');
 });
+
+// route with params
+Route::get('hello/{ten}', function ($ten) {
+    return "Hello " . $ten;
+});
+
+// route with regrex, make sure param should be like regrex below
+Route::get('today/{day}', function ($day) {
+    return 'Today is ' . $day;
+})->where(['day' => '[0-9]{8}']);
