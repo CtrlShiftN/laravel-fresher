5738bec31d6bcf3283490696edf5b4bebf287d69
karuyanakaido24@gmail.com <karuyanakaido24@gmail.com>
2020-03-02 16:04:39 +0700

    Working with Cookie - create and get Signed-off-by: karuyanakaido24@gmail.com <karuyanakaido24@gmail.com>

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -3,30 +3,60 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class MyController extends Controller
 {
     // create a sample controller
-	public function testAction(){
-		echo "This is a controller demo";
-	}
+    public function testAction()
+    {
+        echo "This is a controller demo";
+    }
 
     // test how to send param to controller
-	public function testParams($param)
-	{
-		echo "This param is: " . $param;
-	}
+    public function testParams($param)
+    {
+        echo "This param is: " . $param;
+    }
 
-	// working with URL
-	public function getURL(Request $request){
-		return $request->path(); // return name of the route
-		// return $request->url(); // return the specific url
-		// return $request->is('admin/*'); // return true if url contains 'admin/'
-		// return $request->isMethod('post'); // return true if method is POST
-	}
+    // working with URL
+    public function getURL(Request $request)
+    {
+        return $request->path(); // return name of the route
+        // return $request->url(); // return the specific url
+        // return $request->is('admin/*'); // return true if url contains 'admin/'
+        // return $request->isMethod('post'); // return true if method is POST
+    }
 
-	// get data sent from view postForm.blade.php
-	public function postForm(Request $request){
-		echo $request->fname;
-	}
+    // get data sent from view postForm.blade.php
+    public function postForm(Request $request)
+    {
+        echo $request->fname;
+        // other methods
+        // $request->has('name') // is 'name' param exist?
+        // $request->input('id') // get value from <input name='id'>
+        // $request->input('products.0.name') // get value from array
+        // $request->input('product.name') // get value from json as array
+        // $request->all() // get all values
+        // $request->only('name') // get only param 'name'
+        // $request->except('name') // get all params except param 'name'
+    }
+
+    // set cookie
+    public function setCookie()
+    {
+        $response = new Response();
+        $response->withCookie(
+            'name', // param name
+            'QuanNV', // param value
+            1 // param duration - minute
+        );
+        return $response;
+    }
+
+    // get cookie
+    public function getCookie(Request $request)
+    {
+        return $request->cookie('name');
+    }
 }
